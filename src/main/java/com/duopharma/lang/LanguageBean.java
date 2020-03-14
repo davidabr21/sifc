8ac3e33584bbe6d77b77ad0f9c9b7e507e8f48ea
davidabr21 <62145548+davidabr21@users.noreply.github.com>
2020-03-14 08:53:58 -0500

    Update LanguageBean.java

--- src/main/java/com/duopharma/lang/LanguageBean.java
+++ src/main/java/com/duopharma/lang/LanguageBean.java
@@ -11,7 +11,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
  
 @ManagedBean(name="language")
-@SessionScoped
+@ViewScoped
 public class LanguageBean implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
